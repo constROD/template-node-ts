f0ee26afb7061734ea98e2e9e9c3f03bfb0bf450
constROD <rialcantara21@gmail.com>
2022-02-04 00:06:48 +0800

    chore: add docker

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM node:17-alpine3.14 AS builder
+
+RUN mkdir -p /app/node_modules && chown -R node:node /app
+
+WORKDIR /app
+
+COPY tsconfig.json ./
+COPY package.json ./
+# COPY migration_orm.js ./
+COPY src src
+
+RUN yarn
+
+COPY --chown=node:node . .
+
+RUN yarn build
+
+USER node
+
+FROM node:17-alpine3.14
+
+# Remove this. Once you put this in Environment Variable in the server.
+ENV PORT=3000
+
+COPY --from=builder /app/build ./build
+COPY --from=builder /app/node_modules ./node_modules
+
+CMD ["node", "./build/index.js"]
--- src/App.ts
+++ src/App.ts
@@ -1,12 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { rootRoutes } from './routes';
 import APIMiddleware from './shared/middlewares/Common';
 import swaggerDOC from './swagger';
 
+import compression from 'compression';
 import cors from 'cors';
 import express, { Application } from 'express';
 import swaggerUI from 'swagger-ui-express';
 
-function createServer() {
+const createServer = () => {
   const app: Application = express();
 
   const { tests } = rootRoutes;
@@ -15,6 +17,7 @@ function createServer() {
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  app.use(compression());
 
   /* Docs */
   app.use('/docs', swaggerUI.serve, swaggerUI.setup({ ...swaggerDOC }));
@@ -24,6 +27,6 @@ function createServer() {
   app.use('*', APIMiddleware.notFound);
 
   return app;
-}
+};
 
 export default createServer;
